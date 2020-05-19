9cfa9a882c35f331b69b09ce5bce384ec9cf747a
JiYoung <qnfmtm666@gmail.com>
2020-05-19 22:24:07 +0900

    202005192223 JS HomeScroll

--- main.js
+++ main.js
@@ -3,10 +3,49 @@
 // Make Navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+
 document.addEventListener("scroll", () => {
   if (navbarHeight < window.scrollY) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
+  }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollIntoView(scrollTo);
+});
+
+// Handle click on "contact me" button on home
+
+function scrollIntoView(selector) {
+  console.log(selector);
+  selector.scrollIntoView({ behavior: "smooth" });
+}
+
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener(
+  "click",
+  scrollIntoView.bind(this, document.querySelector("#contact"))
+);
+// Make home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show arrow up button when scrolling down
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHe) {
   }
 });
