395af175f5e232aea802e8f0a0b972fae413b071
JiYoung <qnfmtm666@gmail.com>
2020-05-20 12:54:37 +0900

    2005201254 JS CSS navbar screen

--- main.js
+++ main.js
@@ -23,6 +23,13 @@ navbarMenu.addEventListener("click", (e) => {
   scrollIntoView(link);
 });
 
+// Navbar toggle btn for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  console.log("afd");
+  navbarMenu.classList.toggle("open");
+});
+
 // Handle click on "contact me" button on home
 
 function scrollIntoView(selector) {
